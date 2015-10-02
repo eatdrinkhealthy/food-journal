5971fa965b83b2594418357d2ecca55dd6917a39
stevenjmarsh <sjm.atl@gmail.com>
2015-10-02 13:16:25 -0400

    adding basic routing, with iron router

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,34 +1,34 @@
 if (JournalEntries.find().count() === 0) {
     var je1 = new JournalEntry();
+    je1.set('_id', '2015-09-23');
     je1.set('title', 'good day.');
-    je1.set('date', new Date('2015-09-23'));
     je1.save();
 
     var je2 = new JournalEntry();
     je2.set({
-        title: 'not a very good day.',
-        date: new Date('2015-09-21')
+        _id: '2015-09-21',
+        title: 'not a very good day.'
     });
     je2.save();
 
     var je3 = new JournalEntry();
     je3.set({
-        title: 'getting started again.',
-        date: new Date('2015-09-22')
+        _id: '2015-09-22',
+        title: 'getting started again.'
     });
     je3.save();
 
     var je4 = new JournalEntry();
     je4.set({
-        title: 'beginning cleanse.',
-        date: new Date('2015-08-20')
+        _id: '2015-08-20',
+        title: 'beginning cleanse.'
     });
     je4.save();
 
     var je5 = new JournalEntry();
     je5.set({
-        title: 'standard diet.',
-        date: new Date('2015-08-11')
+        _id: '2015-08-11',
+        title: 'standard diet.'
     });
     je5.save();
 }
