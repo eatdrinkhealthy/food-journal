d745feda6223150cfb63ec7b4240ffbcc784c3c4
stevenjmarsh <sjm.atl@gmail.com>
2015-10-14 11:05:12 -0400

    configured datepicker options/behaviour

--- client/templates/journal_entry_submit.js
+++ client/templates/journal_entry_submit.js
@@ -1,14 +1,26 @@
 Template.journalEntrySubmit.helpers({
-    currentValidDate: function () {
+    newValidDate: function () {
         // TODO: provide validation (unique new date)
-        return moment().format('MM-DD-YYYY');
+        return moment().format('M/D/YY');
     }
 });
 
 Template.journalEntrySubmit.rendered = function() {
     $('#entry-datepicker').datepicker({
         todayBtn: true,
-        format: 'mm/dd/yyyy',
+        autoclose: true,
+        container: '#datepicker-container',
+        format: 'm/d/yy',
         datesDisabled: ['10/05/2015']   // TODO: assign existing entry dates
     });
 };
+
+Template.journalEntrySubmit.events({
+    'click .glyphicon': function(e) {
+        // NOTE, (per the docs) this functionality should work simply by adding
+        // input-group-addon class to the span around the glyph, but wasn't working
+        // ...so hard coded it.
+        e.preventDefault();
+        $('#entry-datepicker').datepicker('show');
+    }
+});
