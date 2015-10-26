9d2ab738c3aaf5fc3a929068000de966a736f7c7
stevenjmarsh <sjm.atl@gmail.com>
2015-10-26 13:50:02 -0400

    limit journal entry list, route param

--- client/templates/journal_entry_list.js
+++
@@ -1,5 +0,0 @@
-Template.journalEntry_list.helpers({
-    journalEntries: function () {
-        return JournalEntries.find({}, {sort: {entryDate: -1}});
-    }
-});
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,6 @@
-Meteor.publish('journalEntries', function () {
-    return JournalEntries.find({ownerId: this.userId});
+Meteor.publish('journalEntries', function (jeLimit) {
+    check (jeLimit, Number);
+
+    return JournalEntries.find({ownerId: this.userId},
+        {sort: {entryDate: -1}, limit: jeLimit});
 });
