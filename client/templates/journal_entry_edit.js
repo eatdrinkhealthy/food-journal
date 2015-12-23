37bacbf645c08b82b25ec6f55db3291b3743f362
stevenjmarsh <sjm.atl@gmail.com>
2015-12-23 14:12:19 -0500

    convert snack after from checkbox to select

--- client/templates/journal_entry_edit.js
+++ client/templates/journal_entry_edit.js
@@ -36,7 +36,8 @@ Template.journalEntry_edit.events({
       'breakfast.food': $(e.target).find('[name=breakfast-food]').val() || null,
       // if breakfast time has a value, combine with date and save, else set to null
       'breakfast.time': formBreakfastTime && combineTimeWithDate(formBreakfastTime, formEntryDate),
-      'breakfast.satisfying': convertYesNoToBooleanOrNull($(e.target).find('[name=breakfast-satisfying]').val())
+      'breakfast.satisfying': convertYesNoToBooleanOrNull($(e.target).find('[name=breakfast-satisfying]').val()),
+      'breakfast.snackAfter': convertYesNoToBooleanOrNull($(e.target).find('[name=breakfast-snack-after]').val())
     });
 
     if (entry.validateAll()) {
